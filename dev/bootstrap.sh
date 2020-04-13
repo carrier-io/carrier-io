e50c8d218cc09842545828c3f99bcd1afbf3adfc
Artem Rozumenko <artyom.rozumenko@gmail.com>
2020-04-13 17:20:37 +0300

    tuned installation of grafana dashboard disabling SSL for demo deployment

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker exec -it carrier-keycloak bash -c "source /tmp/disablessl.sh"
+
 if [[ -z "${GRAFANA_SESSION}" ]]; then
   echo "Please set GRAFANA_SESSION variable"
 else
@@ -13,17 +15,17 @@ else
   docker exec carrier-influx bash -c "influx -execute 'create database profiling'"
 
   # seed grafana datasources
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_comparison | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_jmeter | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_gatling | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_prodsec | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_telegraf | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/loki | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_thresholds | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_profiling | curl -X POST "http://localhost/grafana/api/datasources" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/grafana_dashboards/perfgun_performance_dashboard.json | curl -X POST "http://localhost/grafana/api/dashboards/db" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/grafana_dashboards/perfmeter_dashboards.json | curl -X POST "http://localhost/grafana/api/dashboards/db" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
-  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/grafana_dashboards/telegraph_dashboard.json | curl -X POST "http://localhost/grafana/api/dashboards/db" -u test:test --header "Content-Type: application/json" --header "Cookie: auth_session_id=${GRAFANA_SESSION}" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_comparison | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_jmeter | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_gatling | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_prodsec | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_telegraf | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/loki | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_thresholds | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/influx_datasources/datasource_profiling | curl -X POST "http://localhost/grafana/api/datasources" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/grafana_dashboards/perfgun_performance_dashboard.json | curl -X POST "http://localhost/grafana/api/dashboards/db" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/grafana_dashboards/perfmeter_dashboards.json | curl -X POST "http://localhost/grafana/api/dashboards/db" -u user:user --header "Content-Type: application/json" -d @-
+  curl -s https://raw.githubusercontent.com/carrier-io/carrier-io/master/grafana_dashboards/telegraph_dashboard.json | curl -X POST "http://localhost/grafana/api/dashboards/db" -u user:user --header "Content-Type: application/json" -d @-
 fi
 
 
